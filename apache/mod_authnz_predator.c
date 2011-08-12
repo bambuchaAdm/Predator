c0fa1a89dcced2dfb7a5518e9aecee142c2a73b5
bambucha <bambucha14@gmail.com>
2011-08-12 17:23:03 +0200

    Dodanie funkcji tworzącej podstawowy konfig do serwera.

--- apache/mod_authnz_predator.c
+++ apache/mod_authnz_predator.c
@@ -9,4 +9,13 @@ typedef struct
 ModAuthPredatorConfig;
 
 static
-void * create
+void * createModAuthPredatorConfig(apr_pool_t *p, char *d)
+{
+    ModAuthPredatorConfig * config = apr_palloc(p,sizeof(*config));
+    
+    config->url = NULL;     //Gdzie ma się łączyć
+    config->port = 8443;    //Standardowy port
+    config->service = NULL; //Usługa do której ma być logowany
+    
+    return config;
+}
